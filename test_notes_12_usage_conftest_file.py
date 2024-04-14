e93763dda7d5cbee97ab21748a18e604ff9db719
soorya1235 <108076757+soorya1235@users.noreply.github.com>
2024-04-14 16:56:39 +0530

    Delete test_notes_12_usage_conftest_file.py

--- test_notes_12_usage_conftest_file.py
+++
@@ -1,7 +0,0 @@
-"""
-As of now we are declaring fixtures in side the test file and using it
-by passing as a parameter to the testfunction, if there are common fixtures
-which needs to be used across multiple testcase files, then we defined
-the fixture in the conftest.py file and it will be used in the testcase method.
-
-"""
